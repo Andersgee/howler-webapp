af438509a32a98bf0d84a8ac4559abd08bb5c420
andy <andersgee@gmail.com>
2023-05-05 21:11:18 +0200

    Link

--- src/app/hmm/page.tsx
+++ src/app/hmm/page.tsx
@@ -3,7 +3,9 @@ import { myAction } from "../_actions";
 export default function Page() {
   return (
     <form action={myAction}>
-      <button type="submit">perform server action</button>
+      <button type="submit" className="bg-green-400 px-3 py-2">
+        perform server action
+      </button>
     </form>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   const someEnvVar = process.env.NEXT_PUBLIC_HELLO;
-  return <div className="bg-orange-400">{someEnvVar}</div>;
+  return (
+    <main className="flex justify-center">
+      <div>
+        <div className="">
+          <div>someEnvVar: {someEnvVar}</div>
+          <Link href="/hmm" className="block px-3 py-2 bg-blue-300">
+            GO TO HMM
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
