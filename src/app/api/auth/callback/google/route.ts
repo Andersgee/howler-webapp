55b087a17388cda251a5026ffde648da4e777784
andy <andersgee@gmail.com>
2023-05-07 21:12:10 +0200

    ok, so not way to access the session cookie in the callback route, atleast not with samesite strict? hmm

--- src/app/api/auth/callback/google/route.ts
+++ src/app/api/auth/callback/google/route.ts
@@ -1,17 +1,23 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
+import { SESSION_COOKIE_NAME, USER_COOKIE_NAME } from "src/utils/constants";
+import { cookies } from "next/headers";
 
-const COOKIE_NAME = "__Host-user";
-// this is where user ends up after clicking sign in with google
-// Its important to specify statuscode 303 for NextResponse.redirect
-// https://github.com/vercel/next.js/issues/48204#issuecomment-1512495376
-//
-// more info: status 303 changes method to "GET" (default is status 307 which means keep the same method, in this case "POST")
-// and any page such as "/" only responds to "GET" requests
-
+/**
+ * this is where client is redirected after authorizing with google
+ *
+ * we are at step 3 of openid server flow
+ * https://developers.google.com/identity/openid-connect/openid-connect#confirmxsrftoken
+ * */
 export async function GET(request: NextRequest) {
   try {
     console.log("api/auth/callback/google recieved GET request");
+
     console.log("request.url:", request.url);
+    const allCookies = request.cookies.getAll();
+    console.log("allCookies:", allCookies); //this is empty because request comes from google
+
+    const sessionCookie = cookies().get(SESSION_COOKIE_NAME);
+    console.log("sessionCookie:", sessionCookie);
 
     // that worked fine, we got this:
     // request.url: http://localhost:3000/api/auth/callback/google?state=somesessiontokenhere&code=4%2F0AbUR2VOE6OpPl3gjBfswhjBW9xuV_5AOrSNZptYTe5Ie4CTmAZUbQVJWoaBZh2zRQXiu1Q&scope=email+profile+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&authuser=0&prompt=consent
@@ -30,7 +36,7 @@ export async function GET(request: NextRequest) {
       status: 303,
       headers: {
         Location: `http://localhost:3000`,
-        "Set-Cookie": `${COOKIE_NAME}=${token}; Path=/; Secure; HttpOnly; SameSite=Strict; Max-Age=2592000`,
+        "Set-Cookie": `${USER_COOKIE_NAME}=${token}; Path=/; Secure; HttpOnly; SameSite=Strict; Max-Age=2592000`,
       },
     });
   } catch (error) {
