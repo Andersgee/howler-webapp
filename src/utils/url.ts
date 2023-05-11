ad652e40165ec82c533b9ee78fe56f8046739dbf
andy <andersgee@gmail.com>
2023-05-11 20:49:02 +0200

    lets try edgifying some routes

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,13 +1,13 @@
 /**
  * `new URL()` with encodeURIComponent() on searchParams
  */
-export function urlWithSearchparams(url: string, params: Record<string, string>) {
+export function urlWithSearchparams(url: string, params: Record<string, string | number | boolean>) {
   const u = new URL(url);
   u.search = encodeParams(params);
   return u;
 }
 
-export function encodeParams(params: Record<string, string>) {
+export function encodeParams(params: Record<string, string | number | boolean>) {
   return Object.entries(params)
     .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
     .join("&");
