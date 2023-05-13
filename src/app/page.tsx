84ac0c89f84f187e172ed233c9c0f50f484d6692
andy <andersgee@gmail.com>
2023-05-13 16:27:28 +0200

    some icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { RSCUserprofile } from "./RSCUserprofile";
 import { Userprofile } from "./Userprofile";
 import { hashidFromId } from "src/utils/hashid";
+import { IconGoogle } from "src/icons/Google";
+import { IconGithub } from "src/icons/Github";
+import { IconDiscord } from "src/icons/Discord";
 
 export const metadata = {
   title: "root page",
@@ -56,6 +59,19 @@ export default async function Home() {
         <Link href={`/example/${hashidFromId(5)}`} className="block px-3 py-2 bg-blue-300">
           example 5
         </Link>
+      </div>
+      <div>
+        <h2>Discord</h2>
+        <IconDiscord />
+      </div>
+
+      <div>
+        <h2>Github</h2>
+        <IconGithub />
+      </div>
+      <div>
+        <h2>Google</h2>
+        <IconGoogle />
       </div>
     </main>
   );
