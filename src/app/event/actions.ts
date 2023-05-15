4f5df99a4fe783d16c3852032dd72821639a31d8
andy <andersgee@gmail.com>
2023-05-15 23:25:25 +0200

    sending formdata

--- src/app/event/actions.ts
+++ src/app/event/actions.ts
@@ -1,5 +1,10 @@
 "use server";
 
+//import { redirect } from "next/navigation";
+import { getUserFromCookie } from "src/utils/token";
+
+import { z } from "zod";
+
 /*
 notes to self:
 
@@ -9,6 +14,19 @@ https://nextjs.org/docs/app/building-your-application/data-fetching/server-actio
 all input and output args must be serializable.. so prob use superjson liberally in this file?
  */
 
-export async function myAction() {
+export async function myAction(formData: FormData) {
+  console.log("formData:", formData);
+  const say = formData.get("say");
+  const to = formData.get("to");
+
+  //if (!isValidData) throw new Error('Invalid input.');
+
+  const user = await getUserFromCookie();
+  /*
+  if (!user) {
+    redirect("/profile")
+  }
+  */
+  console.log("user:", user);
   console.log("myAction called");
 }
--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,11 +1,19 @@
 import { myAction } from "./actions";
 
-export function Page() {
+export default function Page() {
   return (
     <div>
       <h1>create event</h1>
       <p>Lorem ipsum dolor sit amet consectetur.</p>
       <form action={myAction}>
+        <div>
+          <label htmlFor="say">What greeting do you want to say?</label>
+          <input name="say" id="say" value="Hi" />
+        </div>
+        <div>
+          <label htmlFor="to">Who do you want to say it to?</label>
+          <input name="to" id="to" value="Mom" />
+        </div>
         <button type="submit" className="bg-blue-500 px-3 py-2 font-bold text-white">
           button with server action
         </button>
