939224da61ea8274e7e1e77d90dfecef1c93fb8a
andy <andersgee@gmail.com>
2023-05-18 13:27:49 +0200

    so serveraction can revalidateTag, but causes hard refresh, maybe put the 'userHasJoined' state (and fetch call) in a separate sc

--- src/app/event/[hashid]/page.tsx
+++ src/app/event/[hashid]/page.tsx
@@ -25,23 +25,19 @@ export default async function Page({ params }: { params: { hashid: string } }) {
           <span className="pr-2 w-16">What?</span>
           <div className="px-2 py-1 flex items-center bg-white dark:bg-black">{event.what || "anything"}</div>
         </div>
-
         <div className="flex items-center gap-1">
           <IconWhere />
           <span className="pr-2 w-16">Where?</span>
           <div className="px-2 py-1 bg-white dark:bg-black">{event.where || "anywhere"}</div>
         </div>
-
         <div className="flex items-center gap-1 ">
           <IconWho />
           <span className="pr-2 w-16">Who?</span>
           <div className="px-2 py-1 bg-white dark:bg-black">{event.who}</div>
         </div>
-
         <div className="flex items-start gap-1">
           <IconWhen />
           <span className="pr-2 w-16">When?</span>
-
           <div className="px-2 py-1 bg-white dark:bg-black">
             <WhenText date={event.when} />
           </div>
