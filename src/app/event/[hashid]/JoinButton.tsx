507e734dae972a48bbc12addeca8b414a131a90f
andy <andersgee@gmail.com>
2023-05-18 13:39:27 +0200

    ok that looks better? only the components is refreshed

---
+++ src/app/event/[hashid]/JoinButton.tsx
@@ -0,0 +1,27 @@
+import { getJoinedUserIds } from "./data";
+import { IconHowler } from "src/icons/Howler";
+
+import { joinOrLeaveEvent } from "./actions";
+import type { TokenUser } from "src/utils/token/schema";
+
+type Props = {
+  eventHashid: string;
+  user: TokenUser | null;
+};
+export async function JoinButton({ user, eventHashid }: Props) {
+  const joinedUserIds = await getJoinedUserIds(eventHashid);
+  const userHasJoined = user && joinedUserIds.includes(user.id);
+
+  return (
+    <form action={joinOrLeaveEvent}>
+      <input type="hidden" name="eventhashid" value={eventHashid} />
+      <button
+        type="submit"
+        className="flex w-40 items-center justify-center rounded-full border-2 border-black bg-blue-50 px-2 py-2 transition-colors hover:bg-blue-200"
+      >
+        <span className="mr-2 text-2xl text-black">{userHasJoined ? "leave" : "join"}</span>
+        <IconHowler />
+      </button>
+    </form>
+  );
+}
--- src/app/event/[hashid]/page.tsx
+++ src/app/event/[hashid]/page.tsx
@@ -1,21 +1,17 @@
 import { notFound } from "next/navigation";
-import { getEvent, getJoinedUserIds } from "./data";
+import { getEvent } from "./data";
 import { WhenText } from "./components";
 import { IconWhat } from "src/icons/What";
 import { IconWhere } from "src/icons/Where";
 import { IconWhen } from "src/icons/When";
 import { IconWho } from "src/icons/Who";
-import { IconHowler } from "src/icons/Howler";
 import { getUserFromCookie } from "src/utils/token";
-import { joinOrLeaveEvent } from "./actions";
+import { JoinButton } from "./JoinButton";
 
 export default async function Page({ params }: { params: { hashid: string } }) {
   const event = await getEvent(params.hashid);
   if (!event) notFound();
   const user = await getUserFromCookie();
-  const joinedUserIds = await getJoinedUserIds(params.hashid);
-
-  const userHasJoined = user && joinedUserIds.includes(user.id);
 
   return (
     <main className="container">
@@ -45,22 +41,8 @@ export default async function Page({ params }: { params: { hashid: string } }) {
             <WhenText date={event.whenEnd} />
           </div>
         </div>
-
-        <div className="my-6 ml-6 flex w-60 flex-col items-center">
-          <p className="mb-1 text-center  text-sm">letsgo</p>
-          {userHasJoined ? <div>you are going</div> : <div>you are not going</div>}
-        </div>
-
-        <form action={joinOrLeaveEvent}>
-          <input type="hidden" name="eventhashid" value={params.hashid} />
-          <button
-            type="submit"
-            className="flex w-40 items-center justify-center rounded-full border-2 border-black bg-blue-50 px-2 py-2 transition-colors hover:bg-blue-200"
-          >
-            <span className="mr-2 text-2xl text-black">{userHasJoined ? "leave" : "join"}</span>
-            <IconHowler />
-          </button>
-        </form>
+        {/* @ts-expect-error Async Server Component */}
+        <JoinButton eventHashid={params.hashid} user={user} />
       </div>
     </main>
   );
