ba32ab99c08ad3ea49d3f3e7d70e0984c5a63133
andy <andersgee@gmail.com>
2023-05-21 17:35:46 +0200

    list latest 10 events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,40 @@
 import Link from "next/link";
+import { db } from "src/db";
+import { formatDate } from "src/utils/date";
+import { hashidFromId } from "src/utils/hashid";
 
 export default async function Page() {
+  const events = await db
+    .selectFrom("Event")
+    .selectAll()
+    .orderBy("id", "desc")
+    .offset(0)
+    .limit(10)
+    .get({
+      cache: "force-cache",
+      next: {
+        tags: ["eventfirst10"],
+      },
+    });
   return (
-    <main className="flex justify-center">
+    <main className="">
       <div className="container bg-orange-400">
         <div>hello</div>
-        <Link href="/event" className="px-3 py-2 bg-blue-500">
-          go to event
-        </Link>
+        <div>
+          <Link href="/event" className="px-3 py-2 bg-blue-500">
+            go to event
+          </Link>
+        </div>
+
+        <div>
+          <ul>
+            {events.map((event) => (
+              <Link prefetch={false} key={event.id} href={`/event/${hashidFromId(event.id)}`}>
+                <li>{`what:${event.what} when:${formatDate(event.when)}`}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
