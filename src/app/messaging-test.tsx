9152a609b0df8a2b795a3005d2b5e32d17a5d849
andy <andersgee@gmail.com>
2023-06-11 19:16:07 +0200

    lets try this

--- src/app/messaging-test.tsx
+++ src/app/messaging-test.tsx
@@ -12,7 +12,10 @@ export function MessagingTest({ className = "" }: Props) {
     <div className={className}>
       <h2>Notification test</h2>
       <p>Foreground notifications (while having app open) should appear here.</p>
-      <p>Background notifications (while its closed or whatever) should NOT appear here</p>
+      <p>
+        Background notifications (while app is closed or not active?) should be proper notifications and not shown here.
+      </p>
+      <p>If notifications are not even turned on there will be a button here instead.</p>
       {!fcmToken && (
         <button
           className="bg-blue-500 px-3 py-2 font-semibold text-white"
@@ -23,11 +26,16 @@ export function MessagingTest({ className = "" }: Props) {
           TURN ON NOTIFICATIONS
         </button>
       )}
-      <ul>
-        {messages.map((message) => (
-          <li key={message.messageId}>{JSON.stringify(message)}</li>
-        ))}
-      </ul>
+      <div className="mb-4">
+        <div>list of foreground messages:</div>
+        <ul>
+          {messages.map((message) => (
+            <li className="my-2" key={message.messageId}>
+              {JSON.stringify(message)}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
