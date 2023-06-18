7e8e9da821de98eb54ac9e93b943eed8dd3608b4
andy <andersgee@gmail.com>
2023-06-18 23:25:51 +0200

    Share icon and fallback to copy-to-clipboard with toast if navigator.share not available

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useToast } from "#src/hooks/use-toast";
+import { IconShare } from "#src/icons/Share";
 import { absUrl } from "#src/utils/url";
 import { Button } from "./ui/Button";
 
@@ -11,26 +13,50 @@ type Props = {
 
 export function ShareButton({ className = "", title }: Props) {
   const pathName = usePathname();
+  const { toast } = useToast();
   return (
     <Button
       variant="secondary"
       className={className}
       onClick={async () => {
+        const url = absUrl(pathName);
         try {
           if (typeof window !== "undefined" && "share" in navigator) {
             await navigator.share({
               title: title,
-              url: absUrl(pathName),
+              url: url,
             });
           } else {
-            console.log("native sharing not supported in your browser, prob show a 'copy link' button instead ");
+            //just copy link instead of native sharing not available
+            const ok = await copyToClipboard(url);
+            if (ok) {
+              toast({
+                description: "Link copied",
+              });
+            }
           }
         } catch (error) {
           console.log(error);
         }
       }}
     >
-      Share
+      <IconShare /> Share
     </Button>
   );
+}
+
+async function copyToClipboard(text: string) {
+  if ("clipboard" in navigator) {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        /* clipboard successfully set */
+        return true;
+      },
+      () => {
+        /* clipboard write failed */
+        return false;
+      }
+    );
+  }
+  return false;
 }
