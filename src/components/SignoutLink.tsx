05d2c1e7482ddcf21e3845adf731d459eb7b13a8
andy <andersgee@gmail.com>
2023-06-28 17:29:56 +0200

    happy with that, lets get back to actual event form now..

--- src/components/SignoutLink.tsx
+++ src/components/SignoutLink.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import type { ComponentProps } from "react";
-import { cn } from "#src/utils/cn";
+//import { cn } from "#src/utils/cn";
 import { Button } from "./ui/Button";
 
 type Props = ComponentProps<"a">;
@@ -10,7 +10,6 @@ type Props = ComponentProps<"a">;
 export function SignoutLink({ children, className, ...props }: Props) {
   const pathname = usePathname();
 
-  //<a href={`/api/auth/signout?route=${pathname}`} className={cn("underline decoration-dotted", className)} {...props}>
   return (
     <Button variant="secondary" asChild>
       <a href={`/api/auth/signout?route=${pathname}`} className={className} {...props}>
