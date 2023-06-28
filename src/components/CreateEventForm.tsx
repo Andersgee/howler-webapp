35e6b2e9692b2805d783a2b6048b7cbdcc7bcc5f
andy <andersgee@gmail.com>
2023-06-28 23:30:28 +0200

    align

--- src/components/CreateEventForm.tsx
+++ src/components/CreateEventForm.tsx
@@ -27,116 +27,111 @@ export function CreateEventForm() {
   const [whenEnd, setWhenEnd] = useState(startOfHour(addHours(new Date(), 2)));
 
   return (
-    <div className="flex flex-col items-center gap-2">
-      {/* What */}
-      <div className="flex items-center">
-        <div className="flex w-24">
-          <IconWhat />
-          <span className="ml-2">What?</span>
-        </div>
-        <Input
-          type="text"
-          name="what"
-          placeholder="anything"
-          className="block"
-          value={what}
-          onChange={(e) => setWhat(e.target.value)}
-        />
-      </div>
-
-      {/* Where */}
-      <div className="flex items-center">
-        <div className="flex w-24">
-          <IconWhere />
-          <span className="ml-2">Where?</span>
-        </div>
-        <Input
-          type="text"
-          name="where"
-          placeholder="anywhere"
-          className="block"
-          value={where}
-          onChange={(e) => setWhere(e.target.value)}
-        />
-      </div>
-
-      {/* Who */}
-      <div className="flex grow-0 items-center">
-        <div className="flex w-24">
-          <IconWho />
-          <span className="ml-2">Who?</span>
-        </div>
-
-        <Input
-          type="text"
-          name="who"
-          placeholder="anyone"
-          className="block"
-          value={who}
-          onChange={(e) => setWho(e.target.value)}
-        />
-      </div>
-
-      {/* When */}
-      <div className="flex">
-        <div className="mt-1.5 flex w-24 grow-0">
-          <IconWhen />
-          <span className="ml-2">When?</span>
-        </div>
-        <div className="grow">
+    <div className="flex justify-center">
+      <div>
+        {/* What */}
+        <div className="my-2 flex items-center">
+          <div className="flex w-24">
+            <IconWhat />
+            <span className="ml-2">What?</span>
+          </div>
           <Input
-            type="datetime-local"
-            name="when"
+            type="text"
+            name="what"
+            placeholder="anything"
             className="block"
-            value={datetimelocalString(when)}
-            onChange={(e) => {
-              if (!e.target.value) return;
-              const newWhen = new Date(e.target.value);
-              setWhen(newWhen);
-              if (whenEnd.getTime() <= newWhen.getTime()) {
-                setWhenEnd(addHours(newWhen, 1));
-              }
-            }}
-          />
-
-          <IconArrowDown height={18} width={18} className="mx-auto my-1" />
-          <Input
-            type="datetime-local"
-            name="whenEnd"
-            className="block"
-            value={datetimelocalString(whenEnd)}
-            onChange={(e) => {
-              if (!e.target.value) return;
-              const newWhenend = new Date(e.target.value);
-              setWhenEnd(newWhenend);
-
-              if (when.getTime() >= newWhenend.getTime()) {
-                setWhen(subHours(newWhenend, 1));
-              }
-            }}
+            value={what}
+            onChange={(e) => setWhat(e.target.value)}
           />
         </div>
-      </div>
-
-      <div className="my-4 flex">
-        <div className="w-24"></div>
-        <div className="flex justify-center">
-          <Button
-            variant="default"
-            disabled={eventCreate.isLoading}
-            className="h-16 rounded-full px-6"
-            onClick={() => {
-              eventCreate.mutate({
-                what: what,
-                where: where,
-                who: who,
-                when: when,
-                whenEnd: whenEnd,
-              });
-            }}
-          >
-            <span className="mr-2 text-2xl">Howl</span> <IconHowler className="h-12 w-12" />
-          </Button>
+        {/* Where */}
+        <div className="my-2 flex items-center">
+          <div className="flex w-24">
+            <IconWhere />
+            <span className="ml-2">Where?</span>
+          </div>
+          <Input
+            type="text"
+            name="where"
+            placeholder="anywhere"
+            className="block"
+            value={where}
+            onChange={(e) => setWhere(e.target.value)}
+          />
+        </div>
+        {/* Who */}
+        <div className="my-2 flex items-center">
+          <div className="flex w-24">
+            <IconWho />
+            <span className="ml-2">Who?</span>
+          </div>
+          <Input
+            type="text"
+            name="who"
+            placeholder="anyone"
+            className="block"
+            value={who}
+            onChange={(e) => setWho(e.target.value)}
+          />
+        </div>
+        {/* When */}
+        <div className="my-2 flex">
+          <div className="mt-1.5 flex w-24 grow-0">
+            <IconWhen />
+            <span className="ml-2">When?</span>
+          </div>
+          <div className="grow">
+            <Input
+              type="datetime-local"
+              name="when"
+              className="block"
+              value={datetimelocalString(when)}
+              onChange={(e) => {
+                if (!e.target.value) return;
+                const newWhen = new Date(e.target.value);
+                setWhen(newWhen);
+                if (whenEnd.getTime() <= newWhen.getTime()) {
+                  setWhenEnd(addHours(newWhen, 1));
+                }
+              }}
+            />
+            <IconArrowDown height={18} width={18} className="mx-auto my-1" />
+            <Input
+              type="datetime-local"
+              name="whenEnd"
+              className="block"
+              value={datetimelocalString(whenEnd)}
+              onChange={(e) => {
+                if (!e.target.value) return;
+                const newWhenend = new Date(e.target.value);
+                setWhenEnd(newWhenend);
+                if (when.getTime() >= newWhenend.getTime()) {
+                  setWhen(subHours(newWhenend, 1));
+                }
+              }}
+            />
+          </div>
+        </div>
+        <div className="my-4 flex">
+          <div className="w-24"></div>
+          <div className="flex justify-center">
+            <Button
+              variant="default"
+              disabled={eventCreate.isLoading}
+              className="h-16 rounded-full px-6"
+              onClick={() => {
+                eventCreate.mutate({
+                  what: what,
+                  where: where,
+                  who: who,
+                  when: when,
+                  whenEnd: whenEnd,
+                });
+              }}
+            >
+              <span className="mr-2 text-2xl">Howl</span> <IconHowler className="h-12 w-12" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
