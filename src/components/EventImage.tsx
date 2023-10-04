9a3afaa884bf425712323341cccaa1514f7c27c1
andy <andersgee@gmail.com>
2023-10-04 18:37:30 +0200

    imagePlaceholder() util

--- src/components/EventImage.tsx
+++ src/components/EventImage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { imagePlaceholder } from "#src/utils/image-shimmer";
 import { imageSizes } from "#src/utils/image-sizes";
 
 type Props = {
@@ -9,7 +10,16 @@ type Props = {
 export function EventImage({ src, alt }: Props) {
   return (
     <div className="relative h-64 w-64 sm:h-96 sm:w-96">
-      <Image src={src} alt={alt} sizes={imageSizes("w-64", { sm: "w-96" })} fill priority className="object-contain" />
+      <Image
+        src={src}
+        alt={alt}
+        sizes={imageSizes("w-64", { sm: "w-96" })}
+        placeholder={imagePlaceholder(256, 256)}
+        fill
+        priority
+        quality={100}
+        className="object-contain"
+      />
     </div>
   );
 }
