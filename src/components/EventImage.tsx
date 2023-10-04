a4a3d8e753d80b49f5eed39ee0a13051d63aab8b
andy <andersgee@gmail.com>
2023-10-04 03:18:54 +0200

    contain with fixed h

--- src/components/EventImage.tsx
+++ src/components/EventImage.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export function EventImage({ src, alt }: Props) {
   return (
-    <div className="relative max-h-64 w-64 sm:max-h-96 sm:w-96">
+    <div className="relative h-64 w-64 sm:h-96 sm:w-96">
       <Image src={src} alt={alt} sizes={imageSizes({ w: 64, sm: 96 })} fill priority className="object-contain" />
     </div>
   );
