75009964d4e29e424515949b85bd0edddc3c1b92
andy <andersgee@gmail.com>
2023-10-17 02:04:11 +0200

    publicProcedure on tiles

--- src/api/routers/tile.ts
+++ src/api/routers/tile.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 import { getTile } from "#src/utils/tags";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const tileRouter = createTRPCRouter({
-  tileLocations: protectedProcedure.input(z.object({ tileId: z.string() })).query(async ({ input }) => {
+  tileLocations: publicProcedure.input(z.object({ tileId: z.string() })).query(async ({ input }) => {
     return getTile({ tileId: input.tileId });
   }),
-  multipleTileLocations: protectedProcedure
-    .input(z.object({ tileIds: z.array(z.string()) }))
-    .query(async ({ input }) => {
-      /*
+  multipleTileLocations: publicProcedure.input(z.object({ tileIds: z.array(z.string()) })).query(async ({ input }) => {
+    /*
       const eventLocations = await db
         .selectFrom("EventLocationTilePivot as p")
         .where("p.tileId", "in", input.tileIds)
@@ -17,7 +15,7 @@ export const tileRouter = createTRPCRouter({
         .selectAll("EventLocation")
         .execute();
       */
-      const locationsList = await Promise.all(input.tileIds.map((tileId) => getTile({ tileId })));
-      return locationsList.flat();
-    }),
+    const locationsList = await Promise.all(input.tileIds.map((tileId) => getTile({ tileId })));
+    return locationsList.flat();
+  }),
 });
