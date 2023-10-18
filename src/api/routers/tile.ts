18103268688726fda030ece4366449ad35cd2ce4
andy <andersgee@gmail.com>
2023-10-18 06:24:17 +0200

    refactor api.tile

--- src/api/routers/tile.ts
+++ src/api/routers/tile.ts
@@ -1,21 +1,52 @@
 import { z } from "zod";
-import { getTile } from "#src/utils/tags";
+import { db } from "#src/db";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
+export const tagsTileRouter = {
+  locations: (p: { tileId: string }) => `tile-locations-${p.tileId}`,
+};
+
 export const tileRouter = createTRPCRouter({
-  tileLocations: publicProcedure.input(z.object({ tileId: z.string() })).query(async ({ input }) => {
-    return getTile({ tileId: input.tileId });
+  locations: publicProcedure.input(z.object({ tileId: z.string() })).query(async ({ input }) => {
+    return db
+      .selectFrom("EventLocationTilePivot")
+      .where("EventLocationTilePivot.tileId", "=", input.tileId)
+      .innerJoin("EventLocation", "EventLocation.id", "EventLocationTilePivot.eventLocationId")
+      .innerJoin("Event", "EventLocation.eventId", "Event.id")
+      .select([
+        "Event.id",
+        "Event.what",
+        "Event.when",
+        "EventLocation.placeName",
+        "EventLocation.lng",
+        "EventLocation.lat",
+      ])
+      .get({
+        next: { tags: [tagsTileRouter.locations({ tileId: input.tileId })] },
+      });
   }),
   multipleTileLocations: publicProcedure.input(z.object({ tileIds: z.array(z.string()) })).query(async ({ input }) => {
-    /*
-      const eventLocations = await db
-        .selectFrom("EventLocationTilePivot as p")
-        .where("p.tileId", "in", input.tileIds)
-        .innerJoin("EventLocation", "EventLocation.id", "p.eventLocationId")
-        .selectAll("EventLocation")
-        .execute();
-      */
-    const locationsList = await Promise.all(input.tileIds.map((tileId) => getTile({ tileId })));
+    const locationsList = await Promise.all(
+      input.tileIds.map((tileId) =>
+        db
+          .selectFrom("EventLocationTilePivot")
+          .where("EventLocationTilePivot.tileId", "=", tileId)
+          .innerJoin("EventLocation", "EventLocation.id", "EventLocationTilePivot.eventLocationId")
+          .innerJoin("Event", "EventLocation.eventId", "Event.id")
+          .select([
+            "Event.id",
+            "Event.what",
+            "Event.when",
+            "EventLocation.placeName",
+            "EventLocation.lng",
+            "EventLocation.lat",
+          ])
+          .get({
+            next: { tags: [tagsTileRouter.locations({ tileId })] },
+          })
+      )
+    );
+
     return locationsList.flat();
   }),
 });
