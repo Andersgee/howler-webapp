b2f4e0469bff5884a4d3c425bd41a39fdc4dab5e
andy <andersgee@gmail.com>
2023-11-01 10:16:57 +0100

    stop errorCreate

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -17,7 +17,7 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   useEffect(() => {
     if (didReport.current) return; //error could change many times, only report first error
 
-    errorCreate.mutate({ name: error.name, message: error.message });
+    //errorCreate.mutate({ name: error.name, message: error.message });
     didReport.current = true;
     // Log the error to an error reporting service
     console.error(error);
